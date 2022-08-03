2fa99ed01cd061e4056672cc03ff55c1bc2e08c3
= <michaelviloria.g@gmail.com>
2022-08-03 12:10:45 -0500

    Componentes y PropTypes sesiones-1-2-3

---
+++ src/components/container/task_list.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { LEVELS } from "../../models/levels.enum";
+import { Task } from "../../models/task.class";
+import TaskComponent from "../pure/task";
+
+const TaskListComponent = () => {
+	const defaultTask = new Task(
+		"Example",
+		"Default description",
+		false,
+		LEVELS.NORMAL
+	);
+
+	// const changeState = (id) => {
+	// 	console.log(id);
+	// };
+
+	return (
+		<div>
+			<div>
+				<h1>Your Tasks:</h1>
+			</div>
+			{/* TODO: Aplicar un for/map para renderizar una lista */}
+			<TaskComponent task={defaultTask} />
+		</div>
+	);
+};
+
+export default TaskListComponent;
---
+++ src/components/pure/greeting.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+
+class Greeting extends Component {
+  render() {
+    return (
+      <div>
+        Hola :D
+      </div>
+    );
+  }
+}
+
+
+Greeting.propTypes = {
+
+};
+
+
+export default Greeting;
